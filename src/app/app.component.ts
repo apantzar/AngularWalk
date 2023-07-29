a16c94034079258bc7e71dba6c13f36b2ace8e26
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2023-07-29 20:30:11 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type:'server-type', name:'server-name', content:'just a test'}];
+  
+
+
+}
